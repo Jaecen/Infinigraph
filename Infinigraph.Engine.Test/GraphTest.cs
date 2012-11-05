a1191938e21f7f6ec23299ac720745d59352eff9
Jason Addington <jason@addington.com>
2012-11-05 13:44:51 -0800

    Implemented basic graph and XML reader.

---
+++ Infinigraph.Engine.Test/GraphTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Infinigraph.Engine.Test
+{
+	public class GraphTest
+	{
+		[Fact]
+		public void Should_Add_Nodes()
+		{
+			var graph0 = new Graph(Enumerable.Empty<KeyValuePair<string, Node>>());
+			var graph1 = graph0.AddNode("one");
+			var graph2 = graph1.AddNode("two");
+
+			Assert.Empty(graph0);
+		
+			Assert.Equal(1, graph1.Count());
+			Assert.True(graph1.ContainsKey("one"));
+			Assert.Equal("one", graph1["one"].Id);
+
+			Assert.Equal(2, graph2.Count());
+			Assert.True(graph2.ContainsKey("one"));
+			Assert.True(graph2.ContainsKey("two"));
+			Assert.Equal("one", graph2["one"].Id);
+			Assert.Equal("two", graph2["two"].Id);
+		}
+
+		[Fact]
+		public void Should_Add_Edges()
+		{
+			var graph0 = new Graph(new[] { 
+				new Node("one", Enumerable.Empty<Edge>()),
+				new Node("two", Enumerable.Empty<Edge>()),
+				new Node("three", Enumerable.Empty<Edge>()),
+			}.ToDictionary(n => n.Id));
+
+			var graph1 = graph0.AddEdge("one", "two", 1, 1);
+			var graph2 = graph1.AddEdge("two", "three", 2, 2);
+
+			Assert.Equal(0, graph0.Sum(kvp => kvp.Value.Edges.Count()));
+
+			Assert.Equal(1, graph1["one"].Edges.Count());
+			Assert.Equal(0, graph1["two"].Edges.Count());
+			Assert.Equal(0, graph1["three"].Edges.Count());
+			Assert.Equal(1, graph1.Sum(kvp => kvp.Value.Edges.Sum(e => e.Length)));
+			Assert.Equal(1, graph1.Sum(kvp => kvp.Value.Edges.Sum(e => e.Length)));
+
+			Assert.Equal(1, graph2["one"].Edges.Count());
+			Assert.Equal(1, graph2["two"].Edges.Count());
+			Assert.Equal(0, graph2["three"].Edges.Count());
+			Assert.Equal(3, graph2.Sum(kvp => kvp.Value.Edges.Sum(e => e.Length)));
+			Assert.Equal(3, graph2.Sum(kvp => kvp.Value.Edges.Sum(e => e.Length)));
+		}
+
+		[Fact]
+		public void Should_Load_Nodes()
+		{
+			var data =
+				@"<graph>
+					<nodes>
+						<node id='one' />
+						<node id='two' />
+						<node id='three' />
+					</nodes>
+				</graph>";
+
+			XmlGraphLoader graphLoader = new XmlGraphLoader();
+			using(var source = new MemoryStream(System.Text.Encoding.Unicode.GetBytes(data)))
+			{
+				var graph = graphLoader.LoadGraph(source);
+
+				Assert.True(graph.ContainsKey("one"));
+				Assert.True(graph.ContainsKey("two"));
+				Assert.True(graph.ContainsKey("three"));
+			}
+		}
+
+		[Fact]
+		public void Should_Load_Edges()
+		{
+			var data =
+				@"<graph>
+					<nodes>
+						<node id='one' />
+						<node id='two' />
+						<node id='three' />
+					</nodes>
+					<edges>
+						<edge start='one' end='two' length='3' rate='1' />
+						<edge start='two' end='three' length='5' rate='2' />
+						<edge start='three' end='one' length='4' rate='3' />
+						<edge start='one' end='three' length='4' rate='4' />
+					</edges>
+				</graph>";
+
+			XmlGraphLoader graphLoader = new XmlGraphLoader();
+			using(var source = new MemoryStream(System.Text.Encoding.Unicode.GetBytes(data)))
+			{
+				var graph = graphLoader.LoadGraph(source);
+
+				Assert.Equal(2, graph["one"].Edges.Count());
+				Assert.Equal(1, graph["two"].Edges.Count());
+				Assert.Equal(1, graph["three"].Edges.Count());
+			}
+		}
+	}
+}
---
+++ Infinigraph.Engine/Graph.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Infinigraph.Engine
+{
+	public class XmlGraphLoader
+	{
+		public Graph LoadGraph(Stream source)
+		{
+			var sourceDoc = XDocument.Load(source);
+
+			var nodes = sourceDoc.Root
+				.Elements("nodes")
+				.Elements("node")
+				.Select(e => e.Attributes("id")
+					.Select(a => a.Value)
+					.FirstOrDefault())
+				.ToDictionary(s => s, s => new Node(s, Enumerable.Empty<Edge>()));
+
+			var edges = sourceDoc.Root
+				.Elements("edges")
+				.Elements("edge")
+				.Select(e => new
+				{
+					Start = e.Attributes("start").Select(a => a.Value).FirstOrDefault(),
+					End = e.Attributes("end").Select(a => a.Value).FirstOrDefault(),
+					Length = e.Attributes("length").Select(a => Decimal.Parse(a.Value)).FirstOrDefault(),
+					Rate = e.Attributes("rate").Select(a => Decimal.Parse(a.Value)).FirstOrDefault(),
+				});
+
+			var graph = edges.Aggregate(new Graph(nodes), (g, e) => g.AddEdge(e.Start, e.End, e.Length, e.Rate));
+
+			return graph;
+		}
+	}
+
+	public class Graph : IReadOnlyDictionary<string, Node>
+	{
+		protected Dictionary<string, Node> Nodes
+		{ get; set; }
+
+		public Node this[string index]
+		{ get { return Nodes[index]; } }
+
+		public Graph(IEnumerable<KeyValuePair<string, Node>> nodes)
+		{
+			if(nodes == null)
+				throw new ArgumentNullException("nodes");
+
+			Nodes = nodes.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+		}
+
+		public Graph(Graph graph, IEnumerable<KeyValuePair<string, Node>> nodes = null)
+			: this(nodes ?? graph.Nodes)
+		{ }
+
+		public Graph AddNode(string id)
+		{
+			if(id == null)
+				throw new ArgumentNullException("id");
+
+			return new Graph(this, Nodes.Concat(new[] { new KeyValuePair<string, Node>(id, new Node(id, Enumerable.Empty<Edge>())) }));
+		}
+
+		public Graph AddEdge(string startId, string endId, decimal length, decimal rate)
+		{
+			if(startId == null)
+				throw new ArgumentNullException("startId");
+
+			if(!Nodes.ContainsKey(startId))
+				throw new ArgumentException(String.Format("No nodes with the ID \"{0}\" exist.", startId), startId);
+
+			if(endId == null)
+				throw new ArgumentNullException("endId");
+
+			if(!Nodes.ContainsKey(endId))
+				throw new ArgumentException(String.Format("No nodes with the ID \"{0}\" exist.", endId), endId);
+
+			var start = new KeyValuePair<string, Node>(
+				startId,
+				new Node(
+					Nodes[startId],
+					edges: Nodes[startId]
+						.Edges
+						.Concat(new[] 
+						{ 
+							new Edge(length, rate, Nodes[endId])
+						})
+				)
+			);
+
+			return new Graph(Nodes
+				.Where(kvp => kvp.Key != startId)
+				.Concat(new[] { start })
+			);
+		}
+
+		public IEnumerator<KeyValuePair<string, Node>> GetEnumerator()
+		{
+			return Nodes.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return Nodes.GetEnumerator();
+		}
+
+		public bool ContainsKey(string key)
+		{
+			return Nodes.ContainsKey(key);
+		}
+
+		public IEnumerable<string> Keys
+		{ get { return Nodes.Keys; } }
+
+		public bool TryGetValue(string key, out Node value)
+		{
+			return Nodes.TryGetValue(key, out value);
+		}
+
+		public IEnumerable<Node> Values
+		{ get { return Nodes.Values; } }
+
+		public int Count
+		{ get { return Nodes.Count; } }
+	}
+
+	public class Node
+	{
+		public string Id
+		{ get; protected set; }
+
+		public IEnumerable<Edge> Edges
+		{ get; protected set; }
+
+		public Node(string id, IEnumerable<Edge> edges)
+		{
+			if(id == null)
+				throw new ArgumentNullException("id");
+
+			if(edges == null)
+				throw new ArgumentNullException("edges");
+
+			Id = id;
+			Edges = edges;
+		}
+
+		public Node(Node node, string id = null, IEnumerable<Edge> edges = null)
+			: this(id ?? node.Id, edges ?? node.Edges)
+		{ }
+	}
+
+	public class Edge
+	{
+		public decimal Length
+		{ get; protected set; }
+
+		public decimal Rate
+		{ get; protected set; }
+
+		public Node Target
+		{ get; protected set; }
+
+		public Edge(decimal length, decimal rate, Node target)
+		{
+			if(length < 0)
+				throw new ArgumentException("Value must be greater than or equal to zero", "length");
+
+			if(rate < 0)
+				throw new ArgumentException("Value must be greater than or equal to zero", "rate");
+
+			if(target == null)
+				throw new ArgumentNullException("target");
+
+			Length = length;
+			Rate = rate;
+			Target = target;
+		}
+
+		public Edge(Edge edge, decimal? length = null, decimal? rate = null, Node target = null)
+			: this(length ?? edge.Length, rate ?? edge.Rate, target ?? edge.Target)
+		{ }
+	}
+}
